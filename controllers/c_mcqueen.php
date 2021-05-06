a1258f5c98c7c79f7d6185a53fe9b0727ce2ddb3
hatrongvu13 <hatrongvu13@gmail.com>
2021-05-07 01:01:37 +0700

    code views call controller and models

--- controllers/c_mcqueen.php
+++ controllers/c_mcqueen.php
@@ -3,7 +3,9 @@ class C_Mcqueen {
     public function ShowMcQueen()
     {
         # code mcqeen page
-        
+        include './models/m_mcqueen.php';
+        $m_mcqueen = new M_Mcqueen();
+        $dsGiay = $m_mcqueen->LayGiayMcqueen();
         #views
         $views = './views/v_GIAY_MOI.php';
         $carousel = './views/item/vi_carousel.php';
