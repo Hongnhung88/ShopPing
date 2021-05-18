8658ea88b54f70ea0b8365701e00ca93ede06419
Hongnhung88 <daohongnhungtd88@gmail.com>
2021-05-18 12:47:52 +0700

    handlder process

--- handler/processRegister.php
+++ handler/processRegister.php
@@ -6,7 +6,7 @@ include '../models/m_Register.php';
 		$email = $_POST["email"];
 		$password = $_POST["password"];
 		$cfpassword = $_POST["cfpassword"];
-		
+		$m_register = new M_Register();
 		//Kiểm tra điều kiện bắt buộc đối với các ô không được bỏ trống
 		if ($email == null || $password == null || $cfpassword == null) {
 			echo "bạn vui lòng nhập đầy đủ thông tin";
